8147a07ca1617580cfce656b4cf99c05fcfb9fc9
Gab Fortin <gabfortin@icloud.com>
2021-12-06 22:30:30 +0000

    Fixed part2

--- day6/part2.py
+++ day6/part2.py
@@ -13,11 +13,14 @@ fishes_init = [int(i) for i in data.split(',')]
 fish_count = len(fishes_init)
 
 def number_of_descendants(x, day):
+	def day_born(i):
+		return day + 1 + x + 7*i
+		
 	number_of_eggs = int(days/(x+max_number_of_incubation))
 	if number_of_eggs > max_number_of_incubation:
 		number_of_eggs = max_number_of_incubation
 	
-	new_fishes_born = [day + 1 + x + 7*i for i in range(0,number_of_eggs+1) if day + 1 + x + 7*i <= days]
+	new_fishes_born = [day_born(i) for i in range(0,number_of_eggs+1) if day_born(i) <= days]
 	
 	global fish_count
 	fish_count += (len(new_fishes_born))
